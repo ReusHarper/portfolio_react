373c6465913f9d73f5a0512cd51923fa71f6bbac
ReusHarper <reusharper@comunidad.unam.mx>
2023-02-17 21:55:33 -0600

    upload components Header y Nav

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+// Dependencies
+import React from "react";
+// Styles
+import './header.css';
+
+const Header = () => {
+	return <div>Header</div>;
+};
+
+export default Header;
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,10 @@
+// Dependencies
+import React from "react";
+// Styles
+import './nav.css';
+
+const Nav = () => {
+	return <div>Nav</div>;
+};
+
+export default Nav;
