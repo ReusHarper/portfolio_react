7b0e82e99c492f8934315e85aa15897517148f6f
ReusHarper <reusharper@comunidad.unam.mx>
2023-02-17 22:35:40 -0600

    upload missing components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+// Dependencies
+import React from "react";
+// Styles
+import './footer.css';
+
+const Footer = () => {
+	return <div>Footer</div>;
+};
+
+export default Footer;
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,10 @@
+// Dependences
+import React from "react";
+// Styles
+import './services.css';
+
+const Services = () => {
+	return <div>Services</div>;
+};
+
+export default Services;
